6d36d18b47d31dd68c189e6f25e8af1207a17ad4
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-08-27 15:44:20 +0200

    update

--- src/components/contactDetail.tsx
+++ src/components/contactDetail.tsx
@@ -1,6 +1,6 @@
 import { FormEvent } from 'react';
 import { deleteContact } from '../services/contacts.ts';
-import Favorite from './favorite.tsx';
+import Favorite from './Favorite.tsx';
 
 export default function ContactDetail() {
   const contact: any = {};
