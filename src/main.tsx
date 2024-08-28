341c1079f5bc3397fd29f814ac4187b9f19dd0c1
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-08-28 08:16:14 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import './styles/index.css';
-import {StrictMode} from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
